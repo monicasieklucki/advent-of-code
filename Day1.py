ad465a28a58a646a30e7e8fb0b66707692c5f797
User <User@DESKTOP-KUIF63D>
2020-12-02 21:54:40 -0600

    day1 part1&2

--- Day1.py
+++ Day1.py
@@ -8,8 +8,13 @@ sum = 2020
 for line in f:
     numbers.append(int(line))
 
+# Part 1 #
+for n1 in numbers:
+    if (2020 - n1) in numbers:
+        print(n1 * (2020 - n1))
+            
+# Part 2 #
 for n1 in numbers:
     for n2 in numbers:
-        if (n1 + n2) == 2020:
-            print(n1 * n2)
-
+        if (2020 - n1 - n2) in numbers:
+            print ((2020 - n1 - n2) * n1 * n2)
